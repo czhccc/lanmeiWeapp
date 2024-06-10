144e39493f7c6b3e349d9422c7be9416c548958d
czh <793210471@qq.com>
2024-06-10 22:52:48 +0800

    订单列表

---
+++ pages/customer/order/order.js
@@ -0,0 +1,50 @@
+// pages/customer/order/order.js
+Page({
+  data: {
+    tabIndex: 0,
+    isShowFilter: false,
+    filterOrderId: '',
+  },
+  onLoad(options) {
+
+  },
+  tabChange(e) {
+    console.log(e)
+    this.setData({
+      tabIndex: e.detail.index
+    })
+  },
+  filter() {
+    this.setData({ isShowFilter: true });
+  },
+  closeFilter() {
+    this.setData({ isShowFilter: false });
+  },
+  cancelOrder() {
+    wx.showModal({
+      title: '确认取消预约？',
+      success(res) {
+        if (res.confirm) {
+          console.log('用户点击确定');
+        } else if (res.cancel) {
+          console.log('用户点击取消');
+        }
+      }
+    });
+  },
+  refund() {
+    wx.showModal({
+      title: '确认退款？',
+      success(res) {
+        if (res.confirm) {
+          console.log('用户点击确定');
+        } else if (res.cancel) {
+          console.log('用户点击取消');
+        }
+      }
+    });
+  },
+  seeDetail() {
+
+  }
+})
