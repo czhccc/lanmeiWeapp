af57fa919e29f74746ccca8bc854d2121d7bbef3
czh <793210471@qq.com>
2024-06-16 01:22:40 +0800

    proceed

--- pages/self/goods/goods.js
+++ pages/self/goods/goods.js
@@ -10,10 +10,42 @@ Page({
     canMail: true, // 是否可以邮寄
     isSale: true, // 是否上架
     status: '', // 当前状态
-    statusArr: ['预订中', '采购中', '销售中', '已完结'], // 当前状态
+    statusArr: [  // 当前状态
+      {value: 'USA', name: '预订中'},
+      {value: 'BRA', name: '采购中'},
+      {value: 'ENG', name: '销售中'},
+      {value: 'FRA', name: '已完结'},
+    ],
+    details: '', // 产品详情
+    editorCtx: null,
+    imgList: [],
+    
   },
   onLoad(options) {
 
+  },
+  onEditorReady() {
+    wx.createSelectorQuery().select('#details').context(res => {
+      console.log('??', res)
+      this.data.editorCtx = res.context;
+      this.getData();
+    }).exec();
+  },
+  getData() {
+    // wx.request({
+    //   url: 'https://your-server-url/api/getRichText', // 替换为你的接口地址
+    //   method: 'GET',
+    //   success: (res) => {
+    //     if (res.statusCode === 200) {
+          
+    //     }
+    //   },
+    // });
+
+    let details = '<p>111111</p>'
+    this.data.editorCtx.setContents({
+      html: details
+    });
   },
   canMailChange(e) { // 是否可以邮寄
     this.setData({
@@ -28,7 +60,37 @@ Page({
   statusChange(e) { // 当前状态
     console.log(e)
     this.setData({
-      status: this.data.statusArr[`${Number(e.detail.value)}`]
+      status: e.detail.value
     })
+  },
+  delete() {
+    wx.showModal({
+      title: '确认删除？',
+      success(res) {
+        if (res.confirm) {
+          console.log('用户点击确定');
+        } else if (res.cancel) {
+          console.log('用户点击取消');
+        }
+      }
+    });
+  },
+  submit() {
+    let that = this;
+    wx.showModal({
+      title: '确认保存？',
+      success(res) {
+        if (res.confirm) {
+          let details = ''
+          that.data.editorCtx.getContents({
+            success(res) {
+              details = res.html
+            }
+          });
+        } else if (res.cancel) {
+          console.log('用户点击取消');
+        }
+      }
+    });
   }
 })
---
+++ pages/self/order/order.js
@@ -0,0 +1,74 @@
+// pages/self/order/order.js
+Page({
+  data: {
+    tabIndex: 0,
+    isShowFilter: false,
+    filterOrderId: '',
+
+    refresherTriggered: false,
+    pageNo: 1,
+    pageSize: 10,
+    listData: [],
+  },
+  onLoad(options) {
+
+  },
+  _getListData() {
+
+  },
+  tabChange(e) {
+    console.log(e)
+    this.setData({
+      tabIndex: e.detail.index
+    })
+  },
+
+  bindrefresherrefresh() {
+    console.log('下拉刷新')
+    this.setData({
+      pageNo: 1,
+      listData: [],
+      refresherTriggered: true
+    })
+    this._getListData()
+  },
+  onScrollToLower() {
+    console.log('触底')
+  },
+
+  filter() {
+    this.setData({ isShowFilter: true });
+  },
+  closeFilter() {
+    this.setData({ isShowFilter: false });
+  },
+  cancelOrder() {
+    wx.showModal({
+      title: '确认取消预订？',
+      success(res) {
+        if (res.confirm) {
+          console.log('用户点击确定');
+        } else if (res.cancel) {
+          console.log('用户点击取消');
+        }
+      }
+    });
+  },
+  refund() {
+    wx.showModal({
+      title: '确认退款？',
+      success(res) {
+        if (res.confirm) {
+          console.log('用户点击确定');
+        } else if (res.cancel) {
+          console.log('用户点击取消');
+        }
+      }
+    });
+  },
+  seeDetail() {
+    wx.navigateTo({
+      url: '/pages/self/order/orderDetail/orderDetail',
+    })
+  }
+})
