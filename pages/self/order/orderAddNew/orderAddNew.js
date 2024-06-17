6b0e42d41b2b8a464dd2d95f84647d74d795a940
DESKTOP-GA73CO4\czh <793210471@qq.com>
2024-06-17 19:01:20 +0800

    proceed

--- pages/self/order/orderAddNew/orderAddNew.js
+++ pages/self/order/orderAddNew/orderAddNew.js
@@ -1,18 +1,39 @@
 // pages/self/order/orderAddNew/orderAddNew.js
 Page({
   data: {
+    num: 1,
     typeArr: [  // 当前状态
-      {value: 'USA', name: '购买'},
-      {value: 'BRA', name: '预订'},
+      {value: 'USA', label: '购买'},
+      {value: 'BRA', label: '预订'},
     ],
     type: '', // 订单类型
+    status: '', // 当前状态
+    statusArr: [  // 当前状态
+      {value: 'USA', label: '预订中'},
+      {value: 'BRA', label: '采购中'},
+      {value: 'ENG', label: '销售中'},
+      {value: 'FRA', label: '已完结'},
+    ],
 
+    name: '',
+    phone: '',
     region: '', // 省市区
+    addressDetail: '', // 详细地址
+
+    pickMethod: null,
+    pickMethodArr: [
+      {value: 'kd', label: '快递'},
+      {value: 'shsm', label: '送货上门'},
+    ],
   },
   onLoad(options) {
 
   },
-
+  numChnage(e) {
+    this.setData({
+      num: e.detail
+    })
+  },
   bindRegionChange(e) { // 选择省市区
     let regions = e.detail.value
     console.log(regions)
@@ -20,4 +41,9 @@ Page({
       region: `${regions[0]}${regions[1]}${regions[2]}`
     })
   },
+  pickMethodChange(e) {
+    this.setData({
+      pickMethod: this.data.pickMethodArray[Number(e.detail.value)]
+    })
+  }
 })
